0b8b1c80bedd1869a14769433ca491225d13b26c
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-01-22 15:38:10 +0700

    login and view profile with jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,33 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AppService } from './app.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor( private appService: AppService ) {}
+    constructor(private authService: AuthService) { }
 
-  // @UseGuards(AuthGuard('local'))
-  @UseGuards(LocalAuthGuard) // ใช้ guard ที่สร้างขึ้นเอง
-  @Post('auth/login')
-  async login(@Request() req) {
-    return req.user;
-  }
+    @UseGuards(LocalAuthGuard) // ใช้ guard ที่สร้างขึ้นเอง
+    @Post('auth/login')
+    async login(@Request() req: any) {
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+        // ?? ยังไม่เข้าใจว่า ถูกส่งไปที่ auth service ได้อย่างไร
+        console.log(req)
+        console.log('>>> user <<<<')
+        console.log(req.user)
+        console.log('app.controller.ts login')
+        return this.authService.login(req.user);
+    }
+
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    getProfile(@Request() req: any) {
+        return req.user;
+    }
+
+    // @Get()
+    // getHello(): string {
+    //   return this.appService.getHello();
+    // }
 }
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -10,6 +10,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+    console.log('local strategy.ts validate')
+
     const user = await this.authService.validateUser(username, password);
     if (!user) {
       throw new UnauthorizedException();
