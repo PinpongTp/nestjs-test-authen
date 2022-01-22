1eee77abcdbac87412abd6c78ee46f39da815d62
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-01-22 14:12:45 +0700

    user first login
    - but not guard
    - but not jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor( private appService: AppService ) {}
+
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
 
   @Get()
   getHello(): string {
