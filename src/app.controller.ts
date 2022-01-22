55c334eabbb95e0fc28a9fa35c58ee1127556b8c
Pinpong Tongpat <pinpong.tp@gmail.com>
2022-01-22 14:20:36 +0700

    login with my guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Request, Post, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor( private appService: AppService ) {}
 
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard) // ใช้ guard ที่สร้างขึ้นเอง
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
